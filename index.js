96f0278f48e53ca09e8b4f6e2ddf29a6c3a6399b
GazJack <greta.jackaite@gmail.com>
2025-03-19 09:58:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// selenium pradžia:
+// npm init
+// npm i selenium-webdriver
+// npm i mocha
+// npm i chai
+// koreguojam package.json -> vietoj     "test": "echo \"Error: no test specified\" && exit 1" ----> irasom     "test": "mocha --timeout 10000"
+// .gitignore
+// susikurti aplanakal - test
+// test -> sukurti testNo1.js (vienas failas vienas testas)
+
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const should = require('chai').should();
