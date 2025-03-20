b4f0fd72cff14ac9a6984f7dfdbb5f0a809cf31c
GazJack <greta.jackaite@gmail.com>
2025-03-20 10:58:57 +0200

    testNo2

--- test/testNo2.js
+++ test/testNo2.js
@@ -7,33 +7,34 @@ describe('should edit specific todo tests', function() {
         await driver.quit();
     })
     
-    it('should edit specific new to do', async function() {
+    it('should edit specific todo', async function() {
         driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://todolist.james.am/');
         await driver.wait(until.elementLocated(By.className('new-todo')),2000);
 
         const newTodoInput = await driver.findElement(By.className('new-todo'));
 
-        await newTodoInput.sendKeys('1 uzduotis', Key.RETURN);
-        await newTodoInput.sendKeys('2 uzduotis', Key.RETURN);
-        await newTodoInput.sendKeys('3 uzduotis', Key.RETURN);
-        await newTodoInput.sendKeys('4 uzduotis', Key.RETURN);
-        await newTodoInput.sendKeys('5 uzduotis', Key.RETURN);
-        await newTodoInput.sendKeys('6 uzduotis', Key.RETURN);
+        await newTodoInput.sendKeys('1 uzduotis',Key.RETURN);
+        await newTodoInput.sendKeys('2 uzduotis',Key.RETURN);
+        await newTodoInput.sendKeys('3 uzduotis',Key.RETURN);
+        await newTodoInput.sendKeys('4 uzduotis',Key.RETURN);
+        await newTodoInput.sendKeys('5 uzduotis',Key.RETURN);
+        await newTodoInput.sendKeys('6 uzduotis',Key.RETURN);
 
         await driver.sleep(1000);
-        const editableToDo = await driver.findElement(By.xpath("//ul[@class='todo-list']//li[contains(., '3 uzduotis')]"));
-
+        const editableToDo = await driver.findElement(By.xpath("/html/body/ng-view/section/section/ul/li[3]"));
         driver.actions().doubleClick(editableToDo).perform();
+
         const editField = await editableToDo.findElement(By.css('input.edit'));
 
-        for(let i=0; i<100; i++) {
+        for(let i=0; i<100;i++) {
             await editField.sendKeys(Key.BACK_SPACE);
-        }
+        };
 
         await editField.sendKeys('Redaguota uzduotis', Key.RETURN);
         const editedItem = await driver.findElement(By.xpath("//ul[@class='todo-list']//li[contains(., 'Redaguota uzduotis')]")).getText();
-        await driver.sleep(10000);
-        assert.strictEqual(editedItem, 'Redaguota uzduotis', 'Todo not edited');
+        assert.strictEqual(editedItem, 'Redaguota uzduotis', 'Todo not edited')
+
     });
 });
+
